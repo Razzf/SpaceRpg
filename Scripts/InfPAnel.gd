8549f15e8320bffb420a6ae739b9c859e33ee0f7
felix <felixsanchez0124@gmail.com>
2020-02-03 15:08:07 -0600

    Inf Panel Interacction added

--- Scripts/InfPAnel.gd
+++ Scripts/InfPAnel.gd
@@ -1,26 +1,38 @@
 extends Panel
 onready var animation = $AnimationPlayer
-onready var infLabel = $Inf
-onready var tiitlePanel = $TittlePanel
+onready var TittleLabel = $TittlePanel/Tittle
+onready var TittlePanel = $TittlePanel
+onready var OkBtn = $OkBtn
+onready var CancelBtn = $CancelBtn
 var _animation = Animation.new()
 
 func _ready():
-	infLabel.hide()
-	tiitlePanel.hide()
+	pass
+	TittleLabel.hide()
+	TittlePanel.hide()
+	OkBtn.hide()
+	CancelBtn.hide()
 
-func _on_ActionButton_pressed():
-	animation.play("PanelInfAppear")
-	yield(animation, "animation_finished")
-	infLabel.show()
-	tiitlePanel.show()
-	
-	yield(get_tree().create_timer(6), "timeout")
-	if animation.is_playing():
-		yield(animation,"animation_finished")
-		infLabel.hide()
-		tiitlePanel.hide()
-		animation.play("PanelInfDisappear")
-	else:
-		infLabel.hide()
-		tiitlePanel.hide()
-		animation.play("PanelInfDisappear")
+func _on_ActionBtn_pressed():
+	animation.play("PanelAppear")
+	yield(animation,"animation_finished")
+	TittlePanel.show()
+	TittleLabel.show()
+	OkBtn.show()
+	CancelBtn.show()
+
+
+func _on_OkBtn_pressed():
+	TittleLabel.hide()
+	TittlePanel.hide()
+	OkBtn.hide()
+	CancelBtn.hide()
+	animation.play("PanelDisappear")
+
+
+func _on_CancelBtn_pressed():
+	TittleLabel.hide()
+	TittlePanel.hide()
+	OkBtn.hide()
+	CancelBtn.hide()
+	animation.play("PanelDisappear")
