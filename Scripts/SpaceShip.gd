e5654ef6ed7224981d5af584eadc3464f821240a
felix <felixsanchez0124@gmail.com>
2020-02-10 10:40:27 -0600

    Unique explosive module animation added

--- Scripts/SpaceShip.gd
+++ Scripts/SpaceShip.gd
@@ -46,10 +46,13 @@ func update_equipped_weapon(w_index) -> void:
 		
 func attack_enemy(_enemy) -> void:
 	if _enemy != null:
-		animation.play("attack")
-		yield(animation,"animation_finished")
+		var i = 0
+		var explosions = equipped_weapon.shooting_scene.instance()
+		for i in range(explosions.max_explosions):
+			animation.play("attack")
+			yield(animation,"animation_finished")
 		equipped_weapon.trigger_counter = 0
-		_enemy.add_child(equipped_weapon.shooting_scene.instance())
+		_enemy.add_child(explosions)
 		_enemy.take_damage(equipped_weapon.power)
 		self.energy -= equipped_weapon.energy_cost
 		emit_signal("weapon_used")
