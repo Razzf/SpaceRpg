e96cfae7db876b300b9002408de0a721b8dfdd05
felix <felixsanchez0124@gmail.com>
2020-03-28 18:39:29 -0500

    Weapon controller script cleaned

--- Scripts/Battle/Control/WeaponController.gd
+++ Scripts/Battle/Control/WeaponController.gd
@@ -13,19 +13,8 @@ onready var UpperButton = $UpDownBtns2/UpButton/icon
 onready var LowerButton = $UpDownBtns2/DownButton/icon
 
 signal weapon_Changed()
-const upwards = -1
-const downwards = 1
-
-var WpnIdxs = [0, 1, 2, 3, 4]
-
-var downdown_index = 0
-var down_index = 1
-var index = 2
-var up_index = 3
-var upup_index = 4
 
 var controllerapeared = true
-var inreact = true
 var chanchange = true
 
 var dragging = false
@@ -37,63 +26,42 @@ func _ready():
 	yield($anim,"animation_finished")
 	controllerapeared = true
 	centerbtn.disabled = false
-	
 	$NamePanel.show()
-	if battle_units.SpaceShip != null and battle_units.SpaceShip.equipped_weapon != null:
-		var ship = battle_units.SpaceShip
-		
-		var dwnbt2 = ship.Weapons[downdown_index].instance()
-		var lowerWeapon = ship.Weapons[down_index].instance()
-		var weapon = ship.equipped_weapon
-		var upperWeapon = ship.Weapons[up_index].instance()
-		var upbt2 = ship.Weapons[upup_index].instance()
-		
-		downbtn2.texture = dwnbt2.icon_texture
-		LowerButton.texture = lowerWeapon.icon_texture
-		weaponIcon.texture = weapon.icon_texture
-		UpperButton.texture = upperWeapon.icon_texture
-		upbtn2.texture = upbt2.icon_texture
-		
-		$NamePanel/NameLabel.text = weapon._name
-		upperWeapon.free()
-		lowerWeapon.free()
-		upbt2.free()
-		dwnbt2.free()
-			
-			
-func update_test(trace):
+	update_weapons()
+
+
+func update_weapons(upwards:bool = true):
 	var ship = battle_units.SpaceShip
 	if ship != null:
-		if ship.find_node("Weapon", true, false) != null:
-			var weapon = ship.get_node("Weapon")
-			if trace == 1:
-				WpnIdxs.push_back(WpnIdxs.front())
-				WpnIdxs.pop_front()
-			else:
-				WpnIdxs.push_front(WpnIdxs.back())
-				WpnIdxs.pop_back()
-				
-			var dwdwwep = ship.Weapons[WpnIdxs[0]].instance()
-			var lowerWeapon = ship.Weapons[WpnIdxs[1]].instance()
-			var upperWeapon = ship.Weapons[WpnIdxs[3]].instance()
-			var upupwep = ship.Weapons[WpnIdxs[4]].instance()
+		if upwards:
+			ship.Weapons.push_front(ship.Weapons.back())
+			ship.Weapons.pop_back()
+		else:
+			ship.Weapons.push_back(ship.Weapons.front())
+			ship.Weapons.pop_front()
 			
+		var dwdwwep = ship.Weapons[0].instance()
+		var lowerWeapon = ship.Weapons[1].instance()
+		var upperWeapon = ship.Weapons[3].instance()
+		var upupwep = ship.Weapons[4].instance()
+		ship.update_equipped_weapon(2)
 		
-			UpperButton.texture = upperWeapon.icon_texture
-			LowerButton.texture = lowerWeapon.icon_texture
-			upbtn2.texture = upupwep.icon_texture
-			downbtn2.texture = dwdwwep.icon_texture
+		UpperButton.texture = upperWeapon.icon_texture
+		LowerButton.texture = lowerWeapon.icon_texture
+		upbtn2.texture = upupwep.icon_texture
+		downbtn2.texture = dwdwwep.icon_texture
+		
+		upperWeapon.free()
+		lowerWeapon.free()
+		upupwep.free()
+		dwdwwep.free()
+		
+		var weapon = ship.equipped_weapon
+		weaponIcon.texture = weapon.icon_texture
+
+		$NamePanel.show()
+		$NamePanel/NameLabel.text = weapon._name
 			
-			upperWeapon.free()
-			lowerWeapon.free()
-			upupwep.free()
-			dwdwwep.free()
-			
-			ship.update_equipped_weapon(WpnIdxs[2])
-			weapon = ship.equipped_weapon
-			$NamePanel.show()
-			$NamePanel/NameLabel.text = weapon._name
-			weaponIcon.texture = weapon.icon_texture
 		
 		
 
@@ -113,24 +81,20 @@ func _on_WeaponIcon_pressed():
 
 
 func _gui_input(event):
-	
 	if event is InputEventScreenTouch:
 		init_posx = event.position.x
 		if !dragging and event.pressed:
 			dragging = true
-
 		if dragging and !event.pressed:
 			chanchange = true
 			dragging = false
-
 	if event is InputEventScreenDrag and dragging and chanchange:
-		if (event.position.x - init_posx) > 40:
-			update_test(downwards)
+		if (event.position.x - init_posx) > 30:
+			update_weapons(false)
 			chanchange = false
 			print("caca")
 			emit_signal("weapon_Changed")
-			
-		elif (init_posx - event.position.x) > 40:
-			update_test(upwards)
+		elif (init_posx - event.position.x) > 30:
+			update_weapons()
 			chanchange = false
 			emit_signal("weapon_Changed")
