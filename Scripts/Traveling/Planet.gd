678ef6f658a73ed25a7c8fd4ee0393effdb89bf0
felix <felixsanchez0124@gmail.com>
2020-04-18 15:03:26 -0500

    shape radius fixed

--- Scripts/Traveling/Planet.gd
+++ Scripts/Traveling/Planet.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+var atmosphere
+var gravity
+var temperature
+var water
+
+
 
 func _ready():
 	randomize()
@@ -8,10 +14,12 @@ func _ready():
 	var b = rand_range(0, 1)
 	var size_cord = int(rand_range(0,10))
 	self.get_node("Sprite").self_modulate = Color(r, g, b, 1)
-	#print(size_cord)
 	self.get_node("Sprite").frame = int(size_cord)
-	self.get_node("Area2D/CollisionShape2D").shape.radius = size_cord + 7
+	self.get_node("Area2D/CollisionShape2D").shape.radius = self.get_node("Sprite").frame + 7
 
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	pass # Replace with function body.
+
+func upd_shape():
+	self.get_node("Area2D/CollisionShape2D").shape.radius = self.get_node("Sprite").frame + 7
--- Scripts/Traveling/Star.gd
+++ Scripts/Traveling/Star.gd
@@ -2,60 +2,31 @@ extends Node2D
 
 const planet_scene = preload("res://Scenes/Traveling/Planet.tscn")
 export(int) var planets_amount 
-var prev_planet_position
-
+export(float, 1, 80) var min_separation
 
 func _ready():
 	for i in range(planets_amount):
 		var planet_to_add = planet_scene.instance()
-		$Planets.add_child(planet_to_add)
+		$Planets.add_child(planet_to_add, true)
 		var sep_pos = create_separate_pos($Planets.get_children())
-		print("esta es la pos:", sep_pos)
 		planet_to_add.global_position = sep_pos
+		
 
-
-	var kk = create_separate_pos($Planets.get_children())
-
-	print("acabo")
-	for planet in $Planets.get_children():
-		print(planet.get_node("Area2D/CollisionShape2D").shape.radius)
 
 func create_separate_pos(prev_planets:Array) -> Vector2:
 	var new_vec = get_rand_vector(-180, 360, 80, 200)
 	var fit = false
-	
-	if prev_planets.size() > 0:
-		while not fit:
-			new_vec = get_rand_vector(-180, 360, 80, 200)
-			for i in range(prev_planets.size()):
-				var prev_pos = prev_planets[i].global_position
-				var diff = new_vec.distance_to(prev_pos)
-				if diff < 80:
-					break
-				if prev_planets[i] == prev_planets.back():
-					fit = true
-		return new_vec
-	else:
-		return new_vec
-				
-	
-	
-	
-#	if prev_planets.size() > 1:
-#
-#		for planet in prev_planets:
-#
-#			var prev_pos = planet.global_position
-#			var diff = new_vec.distance_to(prev_pos)
-#
-#			while diff < 80:
-#				new_vec = get_rand_vector(180, 360, 80, 200)
-#				diff = new_vec.distance_to(prev_pos)
-#
-#			pass
-#
-#	return new_vec
-#	pass
+	while not fit:
+		new_vec = get_rand_vector(-180, 360, 80, 200)
+		for i in range(prev_planets.size()):
+			var prev_pos = prev_planets[i].global_position
+			var diff = new_vec.distance_to(prev_pos)
+			if diff < min_separation:
+				break
+			if prev_planets[i] == prev_planets.back():
+				fit = true
+	return new_vec
+
 
 func get_rand_vector(rangx1, rangx2, rangy1, rangy2) -> Vector2:
 	var randx = rand_range(rangx1, rangx2)
@@ -65,8 +36,6 @@ func get_rand_vector(rangx1, rangx2, rangy1, rangy2) -> Vector2:
 func add_planet():
 	var planet_to_add = planet_scene.instance()
 	$Planets.add_child(planet_to_add)
-	#print(planet_to_add.get_node("Area2D/CollisionShape2D").shape.radius)
-
 
 func _on_SwipeDetector_swiped(direction):
 	if direction == Vector2.RIGHT:
