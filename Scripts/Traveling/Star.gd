0b6f58c74b0569f348b8084ded81d2c3e013b563
felix <felixsanchez0124@gmail.com>
2020-04-17 20:11:33 -0500

    stars particles

--- Scripts/Traveling/Star.gd
+++ Scripts/Traveling/Star.gd
@@ -11,14 +11,14 @@ func _ready():
 		#print(planet_to_add.get_node("Area2D/CollisionShape2D").shape.radius)
 		$Planets.add_child(planet_to_add)
 		var randx = rand_range(-180, 360)
-		var randy = rand_range(50, 130)
+		var randy = rand_range(80, 200)
 		var rand_planet_position = Vector2(randx, randy)
 		if prev_planet_position != null:
 			var diff_btwn_planets = rand_planet_position.distance_to(prev_planet_position)
 			print(diff_btwn_planets)
 			while diff_btwn_planets < 100:
 				randx = rand_range(-180, 360)
-				randy = rand_range(50, 130)
+				randy = rand_range(80, 200)
 				rand_planet_position = Vector2(randx, randy)
 				diff_btwn_planets = rand_planet_position.distance_to(prev_planet_position)
 		planet_to_add.global_position = rand_planet_position
